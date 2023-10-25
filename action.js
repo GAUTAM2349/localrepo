bfa8d6de7d0ec3a53b8c37c157dc397d50315f5e
apnacollege <gautamguddu577@gmail.com>
2023-10-25 14:23:15 +0530

    changed the title of page

---
+++ action.js
@@ -0,0 +1,5 @@
+console.log('hello');
+console.log(document.title);
+document.title = 'changed title';
+
+
--- app.js
+++
@@ -1,3 +0,0 @@
-// this is to upload in sharpener, after creating 
-// a new file name app.js
-// in my previous commit app.js was not present
