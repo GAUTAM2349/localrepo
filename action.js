c42115453fe682bd2da72d41694c22bdddf1439d
apnacollege <gautamguddu577@gmail.com>
2023-10-25 14:28:58 +0530

    given a border to header-title

--- action.js
+++ action.js
@@ -2,4 +2,6 @@ console.log('hello');
 console.log(document.title);
 document.title = 'changed title';
 
+let headTitle = document.getElementById('header-title');
+headTitle.style.borderBottom = '5px solid black';
 
