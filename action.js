028ef342cf1dfdd053c75ff1d649d8395709bb59
apnacollege <gautamguddu577@gmail.com>
2023-10-25 14:37:09 +0530

    changed the font-weight of additems to bold and also it color to green

--- action.js
+++ action.js
@@ -5,3 +5,8 @@ document.title = 'changed title';
 let headTitle = document.getElementById('header-title');
 headTitle.style.borderBottom = '5px solid black';
 
+let addItems = document.getElementById('addItems');
+addItems.style.fontWeight = 'bold';
+addItems.style.color = "green";
+
+
