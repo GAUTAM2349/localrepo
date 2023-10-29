613d59d00ee8071297328ab6099ec16eaffb9005
apnacollege <gautamguddu577@gmail.com>
2023-10-29 13:08:43 +0530

    added hello word before item listener

--- action.js
+++ action.js
@@ -1,60 +1,28 @@
-console.log('hello');
-console.log(document.title);
-document.title = 'changed title';
-
-let headTitle = document.getElementById('header-title');
-headTitle.style.borderBottom = '5px solid black';
-
-let addItems = document.getElementById('addItems');
-addItems.style.fontWeight = 'bold';
-addItems.style.color = "green";
-
-let items = document.getElementsByClassName('list-group-item');
-//changing the first list item and not the second one
-items[0].textContent = 'changed list name';
-items[2].style.backgroundColor = 'green';
-
-for(let i=0;i<items.length;i++){
-    items[i].style.fontWeight = 'bold';
-}
-
-// changing a list manually added with different class name
-
-let myLastList = document.getElementsByClassName('myLastList');
-console.log(myLastList);
-myLastList.textContent = 'my list';
-// it doesn't change anything bcz getElementByClassName() return array of nodes and not specific node
-// we can change it like this, although we shouldn't
-
-myLastList[0].textContent = 'this is my list';
-
-// using getElementsByTagName
-
-let li = document.getElementsByTagName('li');
 
 
-let len = li.length;
-li[len-1].textContent = "my list"
+//traversing DOM 
+let itemList = document.querySelector('#items');
+console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor = 'grey';
+console.log(itemList.parentElement.parentElement);
+console.log(itemList.children);  
+itemList.children[0].style.backgroundColor = 'green'; 
+
+//firstchildelement
 
 
-// making the bgcolor of 2nd item green
+console.log(itemList.firstElementChild);
+console.log(itemList.nextElementSibling);
+console.log(itemList.previousElementSibling);
 
-// let list2 = document.querySelector('.list-group-item:nth-child(2)');
-// list2.style.backgroundColor = 'green';
+// adding  "hello" before item listener
 
-// //making the third item invisible
+let headToInsert1 = document.createElement('h2');
+let tn1 = document.createTextNode("hello");
+headToInsert1.appendChild(tn1);
+let container1 = document.querySelector('header .container');
+let h1 = document.querySelector('header h1');
 
-// let list3 = document.querySelector('.list-group-item:nth-child(3)');
-// list3.style.display = 'none';
+console.log(headToInsert1);
 
-//using queryselectorall to change the font color to green for second item
-
-let listtwo = document.querySelectorAll('.list-group-item');
-listtwo[1].style.color = 'green';
-
-//making the bgcolor of all odd list items green
-let listtwoodd = document.querySelectorAll('.list-group-item:nth-child(odd)');
-
-for(let i=0;i<listtwo.length;i++){
-    listtwoodd[i].style.backgroundColor = 'green';
-}
+container1.insertBefore(headToInsert1,h1);
